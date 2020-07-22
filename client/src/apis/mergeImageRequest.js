0af9cb450ae35ad149676f00d08ce2e219c34037
Aditya Vyas <vyasaditya41@yahoo.in>
2020-07-23 00:57:35 +0530

    chore(comments): add comments to functions and logic

--- client/src/apis/mergeImageRequest.js
+++ client/src/apis/mergeImageRequest.js
@@ -1,5 +1,11 @@
 import { BACKEND_URI } from '../const';
 
+/**
+ * 
+ * @param {*} userImages - The object containing user uploaded image and frame selected by user
+ * @description - The function is used to send the images and recieve the merged image.
+ */
+
 const mergeImageRequest = async (userImages) => {
   const res = await fetch(`${BACKEND_URI}image/mergeImages`, {
     method: 'POST',
